f8c4526f59cd6036a52edb053cfdbf146e227fb1
leonardofernandezalbornoz <a22leonardofa@iessanclemente.net>
2024-03-05 14:29:35 +0100

    segundo commit

---
+++ src/Services/GenerateContent.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services;
+
+
+class GenerateContent{
+
+    private $categories=["Category 1",  "Category 2",  "Category 3"];
+    private $notes=[
+
+        [
+            "description"=>"Note 1"
+        ],
+        ["description"=>"Note 2",
+    ],
+        ["description"=>"Note 3"],
+        ["description"=>"Note 4"],
+        ["description"=>"Note 5"]
+    ]
+
+
+}
